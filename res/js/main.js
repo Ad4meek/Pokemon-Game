db37426142d0cd14b1e188581e8935e70358547a
adamci <adam.erben@seznam.cz>
2022-12-05 10:33:08 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,32 +1,41 @@
-const attack = document.getElementById("attack");
-const pokemon = document.getElementById("pokemon");
-const lives = document.getElementById("lives");
-const lives2 = document.getElementById("lives2");
-const info = document.getElementById("info");
+const attackButton = document.getElementById("attackButton");
+const myPokemon = document.getElementById("myPokemon");
+const enemyPokemon = document.getElementById("enemyPokemon");
+const myHealth = document.getElementById("myHealth");
+const enemyHealth = document.getElementById("enemyHealth");
+const battleInfo = document.getElementById("battleInfo");
 
-attack.onmousedown = () => {
-  attack.style.fontSize = "230%"
-  if (lives.innerHTML > 0 && lives2.innerHTML > 0) {
-    pokemon.style.left = "20%";
-    lives2.innerHTML--;
+
+
+// Battle
+
+attackButton.onmousedown = () => {
+  attackButton.style.fontSize = "230%"
+  if (myHealth.innerHTML > 0 && enemyHealth.innerHTML > 0) {
+    myPokemon.style.left = "20%";
+    enemyHealth.innerHTML--;
     
   }
 
-  if (lives2.innerHTML <= 0) {
-    info.innerHTML = "ENEMY DIED";
+  if (enemyHealth.innerHTML <= 0) {
+    battleInfo.innerHTML = "ENEMY DIED";
     clearInterval(interval);
+    enemyPokemon.style.display = "none";
+    enemyPokemonDeath.style.display = "block";
   }
 };
 
-attack.onmouseup = () => {
-    pokemon.style.left = "14%";
-    attack.style.fontSize = "250%"
+attackButton.onmouseup = () => {
+  myPokemon.style.left = "14%";
+  attackButton.style.fontSize = "250%"
 };
 
 const interval = setInterval(() => {
-    lives.innerHTML--;
-  if (lives.innerHTML <= 0) {
-    info.innerHTML = "YOU DIED";
+  myHealth.innerHTML--;
+  if (myHealth.innerHTML <= 0) {
+    battleInfo.innerHTML = "YOU DIED";
     clearInterval(interval);
+    myPokemon.style.display = "none";
+    myPokemonDeath.style.display = "block";
   }
 }, 500);
