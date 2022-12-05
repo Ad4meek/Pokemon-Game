ec43b485e0fa8dc980ed47b6298a2bc0cdc1b39e
adam <adam.erben@seznam.cz>
2022-12-05 21:19:04 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,39 @@
 const attackButton = document.getElementById("attackButton");
 const myPokemon = document.getElementById("myPokemon");
+const myPokemonDeath = document.getElementById("myPokemonDeath");
 const enemyPokemon = document.getElementById("enemyPokemon");
+const enemyPokemonDeath = document.getElementById("enemyPokemonDeath");
 const myHealth = document.getElementById("myHealth");
 const enemyHealth = document.getElementById("enemyHealth");
 const battleInfo = document.getElementById("battleInfo");
+const battleSite = document.getElementById("battleSite");
+const pokemonSite = document.getElementById("pokemonSite");
+const pokemon = document.getElementById("pokemon");
+const map = document.getElementById("map");
+const cp = document.getElementById("cp");
+const powerUp = document.getElementById("powerUp");
+const back = document.getElementById("back");
 
+back.onclick = () => {
+  pokemonSite.style.display = "block";
+  battleSite.style.display = "none";
+}
 
+let interval
+
+map.onclick = () => {
+  pokemonSite.style.display = "none";
+  battleSite.style.display = "block"; 
+  interval = setInterval(() => {
+    myHealth.innerHTML--;
+    if (myHealth.innerHTML <= 0) {
+      battleInfo.innerHTML = "YOU DIED";
+      clearInterval(interval);
+      myPokemon.style.display = "none";
+      myPokemonDeath.style.display = "block";
+    }
+  }, 500); 
+}
 
 // Battle
 
@@ -30,12 +58,4 @@ attackButton.onmouseup = () => {
   attackButton.style.fontSize = "250%"
 };
 
-const interval = setInterval(() => {
-  myHealth.innerHTML--;
-  if (myHealth.innerHTML <= 0) {
-    battleInfo.innerHTML = "YOU DIED";
-    clearInterval(interval);
-    myPokemon.style.display = "none";
-    myPokemonDeath.style.display = "block";
-  }
-}, 500);
+
